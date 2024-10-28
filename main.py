c15d7d62f27c1eaa56baef72a2dc3db6ed9ac41d
Linus Horn <linus@linushorn.dev>
2024-10-28 19:29:46 +0100

    better working prototype

--- main.py
+++ main.py
@@ -4,11 +4,15 @@ from fastapi.requests import Request
 import uvicorn
 import requests
 import json
+import bs4
 
 app = FastAPI()
 
-with open("known_vpn.json", "r") as f:
-    known_vpn = json.load(f)
+with open("./cache/known_vpn.json", "r") as f:
+    known_vpn_cache = json.load(f)
+
+with open("./cache/web_autocomplete.json", "r") as f:
+    web_autocomplete_cache = json.load(f)
 
 with open("vpnapi.token", "r") as f:
     API_KEY = f.read().strip()
@@ -23,7 +27,7 @@ async def read_root(request: Request):
 @app.get("/is_vpn", response_class=JSONResponse)
 async def is_vpn(ip: str):
 
-    if ip in known_vpn:
+    if ip in known_vpn_cache:
         return {"is_vpn": True}
 
     url = f"https://vpnapi.io/api/{ip}?key={API_KEY}"
@@ -33,13 +37,32 @@ async def is_vpn(ip: str):
 
     # if (data.security.vpn || data.security.proxy || data.security.tor || data.security.relay)
     if data["security"]["vpn"] or data["security"]["proxy"] or data["security"]["tor"] or data["security"]["relay"]:
-        known_vpn.append(ip)
-        with open("known_vpn.json", "w") as f:
-            json.dump(known_vpn, f)
+        known_vpn_cache.append(ip)
+        with open("/cache/known_vpn.json", "w") as f:
+            json.dump(known_vpn_cache, f)
 
         return {"is_vpn": True}
     else:
         return {"is_vpn": False}
 
+@app.get("/web_autocomplete", response_class=JSONResponse)
+async def web_autocomplete(q: str):
+
+    if q in web_autocomplete_cache.keys():
+        return web_autocomplete_cache[q]
+
+
+    url = f"https://duckduckgo.com/ac/?q={q}&kl=wt-wt"
+    response = requests.get(url)
+    data = response.json()
+
+    web_autocomplete_cache[q] = data
+
+    with open("./cache/web_autocomplete.json", "w") as f:
+        json.dump(web_autocomplete_cache, f)
+
+    return data
+
+
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=80)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
